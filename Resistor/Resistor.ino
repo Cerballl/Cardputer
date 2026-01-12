c790aa72f303d9f19e54208f3772fe3d737be287
Cerballl <114355871+Cerballl@users.noreply.github.com>
2026-01-12 14:40:32 +0200

    rewrited code for cardputer ADV

--- Resistor/Resistor.ino
+++ Resistor/Resistor.ino
@@ -98,103 +98,104 @@ void loop() {
 
    if (M5Cardputer.Keyboard.isChange())
       if (M5Cardputer.Keyboard.isPressed()) {
-            if (M5Cardputer.Keyboard.isKeyPressed('/')) 
-   {
-      M5Cardputer.Speaker.tone(4000,50);
-      M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_BLACK);
-     
-     band++;
-     if(band>3)
-     band=0; 
-     M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_WHITE);
-    }
+        Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
+            
+            for (auto i : status.word){
+              if (status.word.size() == 1 && status.word[0] == '/') {
+                M5Cardputer.Speaker.tone(4000,50);
+                M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_BLACK);
+              
+                band++;
+                if(band>3)
+                band=0; 
+                M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_WHITE);
+              }
 
-             if (M5Cardputer.Keyboard.isKeyPressed(',')) 
-   {
-      M5Cardputer.Speaker.tone(4000,50);
-      M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_BLACK);
-     
-     band--;
-     if(band<0)
-     band=3; 
-     M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_WHITE);
-    }
-    
+              if ((status.word.size() == 1 && status.word[0] == ',')) {
+                M5Cardputer.Speaker.tone(4000,50);
+                M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_BLACK);
+              
+                band--;
+                if(band<0)
+                band=3; 
+                M5Cardputer.Display.fillCircle(bPos[band]+2,58,2,TFT_WHITE);
+              }
+              
 
-     if (M5Cardputer.Keyboard.isKeyPressed('.')) 
-     {
-     M5Cardputer.Speaker.tone(3600,50);
-     if(band==0)
-      {
-         M5Cardputer.Display.fillCircle(16,(first*13)+5,2,TFT_BLACK);
-      first++;
-      if(first>9)
-      first=0;
-      M5Cardputer.Display.fillRect(x+30,y,6,26,colors[first]);
-       M5Cardputer.Display.fillCircle(16,(first*13)+5,2,TFT_WHITE);
-      }
-          if(band==1)
-            {
-              M5Cardputer.Display.fillCircle(38,(secon*13)+5,2,TFT_BLACK);
-         secon++;
-         if(secon>9)
-         secon=0;
-         M5Cardputer.Display.fillCircle(38,(secon*13)+5,2,TFT_WHITE);
-         M5Cardputer.Display.fillRect(x+42,y+3,6,20,colors[secon]);
+              if ((status.word.size() == 1 && status.word[0] == '.')) {
+                M5Cardputer.Speaker.tone(3600,50);
+                if(band==0)
+                  {
+                    M5Cardputer.Display.fillCircle(16,(first*13)+5,2,TFT_BLACK);
+                    first++;
+                    if(first>9)
+                    first=0;
+                    M5Cardputer.Display.fillRect(x+30,y,6,26,colors[first]);
+                    M5Cardputer.Display.fillCircle(16,(first*13)+5,2,TFT_WHITE);
+                  }
+                  if(band==1)
+                    {
+                      M5Cardputer.Display.fillCircle(38,(secon*13)+5,2,TFT_BLACK);
+                      secon++;
+                      if(secon>9)
+                      secon=0;
+                      M5Cardputer.Display.fillCircle(38,(secon*13)+5,2,TFT_WHITE);
+                      M5Cardputer.Display.fillRect(x+42,y+3,6,20,colors[secon]);
+                    }
+
+                    if(band==2)
+                    {
+                      M5Cardputer.Display.fillCircle(188,(multiplier*11)+5,2,TFT_BLACK);
+                      multiplier++;
+                      if(multiplier>11)
+                      multiplier=0;
+                      M5Cardputer.Display.fillCircle(188,(multiplier*11)+5,2,TFT_WHITE);
+                      M5Cardputer.Display.fillRect(x+54,y+3,6,20,colors[multiplier]);
+                    }
+
+                    if(band==3)
+                    {
+                      M5Cardputer.Display.fillCircle(61+(tolerance*34),133,2,TFT_BLACK);
+                      tolerance++;
+                      if(tolerance>3)
+                      tolerance=0;
+                      M5Cardputer.Display.fillCircle(61+(tolerance*34),133,2,TFT_WHITE);
+                      M5Cardputer.Display.fillRect(x+82,y,6,26,cT[tolerance]);
+                    }
+
+                double res=((first*10)+secon)*multis[multiplier];
+                M5Cardputer.Display.fillRect(44,70,136,46,0x00A5);
+                double maxx=res+(res*(tt[tolerance]/100.00));
+                double minn=res-(res*(tt[tolerance]/100.00));
+                M5Cardputer.Display.setTextColor(TFT_WHITE,0x00A5); 
+
+                if(res>=1000 && res<1000000){
+                  M5Cardputer.Display.drawString(String(first)+String(secon)+"x10 ="+String(res/1000)+" K",48,76,2);
+                  M5Cardputer.Display.drawString(String(multiplier),85,72);
+                  M5Cardputer.Display.drawString("MAX:"+String(maxx),48,94);
+                  M5Cardputer.Display.drawString("MIN:"+String(minn),48,106);
+                }
+                
+                  else if(res>=1000000)
+                {
+                  M5Cardputer.Display.drawString(String(first)+String(secon)+"x10 ="+String(res/100000)+" M",48,76,2);
+                  M5Cardputer.Display.drawString(String(multiplier),85,72);
+                  M5Cardputer.Display.drawString("MAX:"+String(maxx),48,94);
+                  M5Cardputer.Display.drawString("MIN:"+String(minn),48,106);
+                }
+                
+                else
+                {
+                M5Cardputer.Display.drawString(String(first)+String(secon)+"x10 ="+String(res)+" ohm",48,76,2);
+                M5Cardputer.Display.drawString(String(multiplier),85,72);
+                M5Cardputer.Display.drawString("MAX:"+String(maxx),48,94);
+                M5Cardputer.Display.drawString("MIN:"+String(minn),48,106);
+                }
+
+                M5Cardputer.Display.setTextColor(TFT_WHITE,TFT_BLACK); 
+                
+              }
             }
-
-             if(band==2)
-            {
-              M5Cardputer.Display.fillCircle(188,(multiplier*11)+5,2,TFT_BLACK);
-          multiplier++;
-          if(multiplier>11)
-          multiplier=0;
-          M5Cardputer.Display.fillCircle(188,(multiplier*11)+5,2,TFT_WHITE);
-          M5Cardputer.Display.fillRect(x+54,y+3,6,20,colors[multiplier]);
-            }
-
-                if(band==3)
-            {
-              M5Cardputer.Display.fillCircle(61+(tolerance*34),133,2,TFT_BLACK);
-          tolerance++;
-          if(tolerance>3)
-          tolerance=0;
-          M5Cardputer.Display.fillCircle(61+(tolerance*34),133,2,TFT_WHITE);
-          M5Cardputer.Display.fillRect(x+82,y,6,26,cT[tolerance]);
-            }
-
-     double res=((first*10)+secon)*multis[multiplier];
-     M5Cardputer.Display.fillRect(44,70,136,46,0x00A5);
-     double maxx=res+(res*(tt[tolerance]/100.00));
-     double minn=res-(res*(tt[tolerance]/100.00));
-     M5Cardputer.Display.setTextColor(TFT_WHITE,0x00A5); 
-
-     if(res>=1000 && res<1000000){
-     M5Cardputer.Display.drawString(String(first)+String(secon)+"x10 ="+String(res/1000)+" K",48,76,2);
-     M5Cardputer.Display.drawString(String(multiplier),85,72);
-     M5Cardputer.Display.drawString("MAX:"+String(maxx),48,94);
-     M5Cardputer.Display.drawString("MIN:"+String(minn),48,106);
-     }
-     
-      else if(res>=1000000)
-     {
-     M5Cardputer.Display.drawString(String(first)+String(secon)+"x10 ="+String(res/100000)+" M",48,76,2);
-     M5Cardputer.Display.drawString(String(multiplier),85,72);
-     M5Cardputer.Display.drawString("MAX:"+String(maxx),48,94);
-     M5Cardputer.Display.drawString("MIN:"+String(minn),48,106);
-     }
-     
-     else
-     {
-     M5Cardputer.Display.drawString(String(first)+String(secon)+"x10 ="+String(res)+" ohm",48,76,2);
-     M5Cardputer.Display.drawString(String(multiplier),85,72);
-     M5Cardputer.Display.drawString("MAX:"+String(maxx),48,94);
-     M5Cardputer.Display.drawString("MIN:"+String(minn),48,106);
-     }
-
-     M5Cardputer.Display.setTextColor(TFT_WHITE,TFT_BLACK); 
-     
-    }
     
       }
  
